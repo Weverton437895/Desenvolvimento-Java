fca06ef58e552d3a6c5fc28626f5f9c12bb895f1
Weverton437895 <wevertontevez0@gmail.com>
2026-02-18 20:39:47 -0300

    Desenvolvimento-Java

---
+++ Tecnica-programação-I/ATIVIDADE PRÁTICA 7 - HERANÇA/view/Main2.java
@@ -0,0 +1,24 @@
+package view;
+
+import modelo.PagamentoCartao;
+
+import modelo.PagamentoDinheiro;
+
+public class Main2 {
+
+	public static void main(String args[]) {
+		
+	
+		double valorProduto = 100.0;
+
+		PagamentoDinheiro pagamentoDinheiro = new PagamentoDinheiro(valorProduto);
+	    System.out.println("Pagamento em Dinheiro");
+	    System.out.println(pagamentoDinheiro.emitirRecibo());
+
+	    System.out.println();
+
+	    PagamentoCartao pagamentoCartao = new PagamentoCartao(valorProduto);
+	    System.out.println("Pagamento com Cartão");
+	    System.out.println(pagamentoCartao.emitirRecibo());
+	}
+}
